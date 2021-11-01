cb4274747baa260a976d8e91c9134a75ea42f60a
Martin <Lapetajemartin@gmail.com>
2021-11-01 16:01:14 +0800

    added delete successful modal

--- 4-Portal/src/app/screens/home/table/table.component.ts
+++ 4-Portal/src/app/screens/home/table/table.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -11,12 +11,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class TableComponent implements OnInit {
   searchValue: string = '';
-  clearSearch() {
-    this.searchValue = '';
-  }
 
   readonly PATH_ALL = '/user/all';
   readonly PATH_SEARCH = '/user/search/';
+  readonly PATH_DELETE = '/user/delete/';
 
   apiResult = Object.keys;
   users: any[] = [];
@@ -32,12 +30,20 @@ export class TableComponent implements OnInit {
 
   constructor(private api: HttpClient, private modalService: NgbModal) {}
 
-  async ngOnInit() {
-    this.displayAllUsers();
+  clearSearch() {
+    this.searchValue = '';
   }
 
   rowSelected(row: any) {
     this.userSelected = row;
+  }
+
+  async delete(): Promise<any> {
+    await this.api.get(environment.API_URL + this.userSelected.id).toPromise();
+  }
+
+  async ngOnInit() {
+    this.displayAllUsers();
   }
 
   async searchClear() {
@@ -46,10 +52,14 @@ export class TableComponent implements OnInit {
 
   async search(query: string) {
     if (query.trim() == '') return;
-    this.getResult(await this.searchTerm(query));
+    this.getResult(await this.getSearch(query));
   }
 
-  private async searchTerm(term: string): Promise<any> {
+  private async displayAllUsers() {
+    this.getResult(await this.getUsers());
+  }
+
+  private async getSearch(term: string): Promise<any> {
     try {
       return await this.api
         .get(environment.API_URL + this.PATH_SEARCH + term)
@@ -57,10 +67,6 @@ export class TableComponent implements OnInit {
     } catch (error) {
       console.log(error);
     }
-  }
-
-  private async displayAllUsers() {
-    this.getResult(await this.getUsers());
   }
 
   private async getUsers(): Promise<any> {
@@ -97,4 +103,56 @@ export class TableComponent implements OnInit {
     fcPassword: new FormControl('', Validators.required),
     fcPassword2: new FormControl('', Validators.required),
   });
+
+  alerts!: Alert[];
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
+  // style="z-index: 999; width: 100%;"
+  success = 'success';
 }
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [
+  {
+    type: 'success',
+    message: 'This is an success alert',
+  },
+  {
+    type: 'info',
+    message: 'This is an info alert',
+  },
+  {
+    type: 'warning',
+    message: 'This is a warning alert',
+  },
+  {
+    type: 'danger',
+    message: 'This is a danger alert',
+  },
+  {
+    type: 'primary',
+    message: 'This is a primary alert',
+  },
+  {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  },
+  {
+    type: 'light',
+    message: 'This is a light alert',
+  },
+  {
+    type: 'dark',
+    message: 'This is a dark alert',
+  },
+];

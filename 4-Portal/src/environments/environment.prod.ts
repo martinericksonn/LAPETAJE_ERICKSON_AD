738f2fec0b319035bbfdbd5b5a12e0de3ff4472d
Martin <Lapetajemartin@gmail.com>
2021-11-03 20:16:33 +0800

    add min height

--- 4-Portal/src/environments/environment.prod.ts
+++ 4-Portal/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://localhost:5000/appsdev-79e1d/us-central1/api',
 };
--- 4-Portal/src/environments/environment.ts
+++ 4-Portal/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://localhost:5000/appsdev-79e1d/us-central1/api',
 };
 
 /*

e717a234fd42fefa325a22ad72536a0c215197f7
Martin <Lapetajemartin@gmail.com>
2021-11-26 17:31:26 +0800

    Chat app added welcome screen

--- Chat-App/src/app/app.module.ts
+++ Chat-App/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './screens/welcome/welcome.component';
+import { LoginComponent } from './screens/login/login.component';
+import { SignUpComponent } from './screens/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

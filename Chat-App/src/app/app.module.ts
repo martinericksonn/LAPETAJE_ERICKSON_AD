5ab96778e889ef178832c8fe4a9fde5e6532a070
Martin <Lapetajemartin@gmail.com>
2021-11-26 21:20:27 +0800

    login page

--- Chat-App/src/app/app.module.ts
+++ Chat-App/src/app/app.module.ts
@@ -8,21 +8,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeComponent } from './screens/welcome/welcome.component';
 import { LoginComponent } from './screens/login/login.component';
 import { SignUpComponent } from './screens/sign-up/sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
   ],
+
   imports: [
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDividerModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Chat-App/src/app/screens/login/login.component.ts
+++ Chat-App/src/app/screens/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
 
-  constructor() { }
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
 
-  ngOnInit(): void {
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
